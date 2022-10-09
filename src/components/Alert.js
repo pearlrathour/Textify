ca7bee5b91a01460fc619ad54194308dbcc2fac6
Pearl Rathour <99817664+pearlrathour@users.noreply.github.com>
2022-10-09 22:22:50 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Alert(props) {
     const capitalize = (word)=>{
-        const lower = word.tolowerCase();
+        const lower = word.toLowerCase();
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
     return (
